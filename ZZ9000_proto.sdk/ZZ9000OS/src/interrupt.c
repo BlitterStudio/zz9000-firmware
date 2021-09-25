c348c94af5554758944e964ac12f8d3af1c1d249
Lukas F. Hartmann <lukas@mntre.com>
2021-09-25 17:30:13 +0200

    move exception init to interrupt.c (was in ethernet)

--- ZZ9000_proto.sdk/ZZ9000OS/src/interrupt.c
+++ ZZ9000_proto.sdk/ZZ9000OS/src/interrupt.c
@@ -8,20 +8,31 @@ XScuGic* interrupt_get_intc() {
 }
 
 int interrupt_configure() {
-  int result;
-  XScuGic *intc = interrupt_get_intc();
-  XScuGic_Config *intc_config;
+	int result;
+	XScuGic *intc = interrupt_get_intc();
+	XScuGic_Config *intc_config;
 
-  intc_config = XScuGic_LookupConfig(XPAR_PS7_SCUGIC_0_DEVICE_ID);
-  if (!intc_config) {
-    return XST_FAILURE;
-  }
+	intc_config = XScuGic_LookupConfig(XPAR_PS7_SCUGIC_0_DEVICE_ID);
+	if (!intc_config) {
+		return XST_FAILURE;
+	}
 
-  printf("XScuGic_CfgInitialize()\n");
-  result = XScuGic_CfgInitialize(intc, intc_config, intc_config->CpuBaseAddress);
+	printf("XScuGic_CfgInitialize()\n");
+	result = XScuGic_CfgInitialize(intc, intc_config, intc_config->CpuBaseAddress);
 
-  if (result != XST_SUCCESS) {
-    return result;
-  }
+	if (result != XST_SUCCESS) {
+		return result;
+	}
+
+	Xil_ExceptionInit();
+
+	/*
+	* Connect the interrupt controller interrupt handler to the hardware
+	* interrupt handling logic in the processor.
+	*/
+	Xil_ExceptionRegisterHandler(XIL_EXCEPTION_ID_IRQ_INT,
+		(Xil_ExceptionHandler)XScuGic_InterruptHandler,
+		intc);
+
   return 0;
 }
