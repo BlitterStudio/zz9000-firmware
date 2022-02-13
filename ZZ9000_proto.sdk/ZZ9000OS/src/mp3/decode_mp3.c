6758af486f59c29f12ac5983d949711cce9ceb4b
Christian Heller <c.heller@plomlompom.de>
2022-02-13 21:58:58 +0100

    Remove heap allocation from decode_mp3, expect outside provision of output_buffer and its size.

--- ZZ9000_proto.sdk/ZZ9000OS/src/mp3/decode_mp3.c
+++ ZZ9000_proto.sdk/ZZ9000OS/src/mp3/decode_mp3.c
@@ -8,35 +8,33 @@
 // TODO: adapt decode_mp3 interface to more specific requirements ("call a decode_mp3() function via a ZZ9000 register giving source and dest (uint32_t) pointers")
 // TODO: status register where Amiga can pull result (OK/Error/busy)
 
-void * decode_mp3(char * input_buffer, int * buffer_size) {
+int decode_mp3(char * input_buffer, size_t input_buffer_size, char * output_buffer, size_t output_buffer_size) {
     mp3dec_ex_t mp3d;
     mp3dec_frame_info_t frame_info;
     memset(&frame_info, 0, sizeof(frame_info));
-    int ret = mp3dec_ex_open_buf(&mp3d, input_buffer, *buffer_size, 0);
+    // sets up input_buffer as mp3d->file.buffer
+    int ret = mp3dec_ex_open_buf(&mp3d, input_buffer, input_buffer_size, 0);
     if (ret) {
       printf("mp3dec_ex_open_buf failed: %d\n", ret);
       exit(ret);
     }
-    mp3d_sample_t * output_buffer = NULL;
     size_t offset_new_samples = 0;
-    size_t output_buffer_size = 0;
     int max_samples = UINT_MAX;
+    memset(output_buffer, 0, output_buffer_size);
     while (1) {
-        mp3d_sample_t * frame_buffer = NULL;
-        size_t read_samples = mp3dec_ex_read_frame(&mp3d, &frame_buffer, &frame_info, max_samples);
+        // this will point into mp3d->buffer, which is defined on the stack
+        // as mp3d_sample_t buffer[MINIMP3_MAX_SAMPLES_PER_FRAME]
+        mp3d_sample_t * pcm_buffer = NULL;
+        size_t read_samples = mp3dec_ex_read_frame(&mp3d, &pcm_buffer, &frame_info, max_samples);
         if (!read_samples) {
             break;
         }
-        size_t new_frame_size = read_samples * sizeof(mp3d_sample_t);
-        output_buffer_size += new_frame_size;
-        output_buffer = realloc(output_buffer, output_buffer_size);
-        if (!output_buffer) {
-          printf("memory allocation for mp3 decoder output failed");
-          exit(1);
+	if (offset_new_samples + (read_samples * sizeof(mp3d_sample_t)) >= output_buffer_size) {
+            break;
         }
-        memcpy(output_buffer + offset_new_samples, frame_buffer, new_frame_size);
-        offset_new_samples += read_samples;
+        size_t new_frame_size = read_samples * sizeof(mp3d_sample_t);
+        memcpy(output_buffer + offset_new_samples, pcm_buffer, new_frame_size);
+        offset_new_samples += read_samples * sizeof(mp3d_sample_t);
     }
-    *buffer_size = output_buffer_size;
-    return output_buffer;
+    return offset_new_samples;
 }
