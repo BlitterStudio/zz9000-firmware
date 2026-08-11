6213a0026d0976f9eb450509953d5e332661e3bd
Dimitris Panokostas <midwan@gmail.com>
2026-08-11 20:27:50 +0200

    build(fpga): target high-delay timing paths

--- zz9000_project.tcl
+++ zz9000_project.tcl
@@ -1645,9 +1645,9 @@ current_run -synthesis [get_runs synth_1]
 
 # Create 'impl_1' run (if not found)
 if {[string equal [get_runs -quiet impl_1] ""]} {
-    create_run -name impl_1 -part xc7z020clg400-1 -flow {Vivado Implementation 2018} -strategy "Performance_ExplorePostRoutePhysOpt" -report_strategy {No Reports} -constrset constrs_1 -parent_run synth_1
+    create_run -name impl_1 -part xc7z020clg400-1 -flow {Vivado Implementation 2018} -strategy "Performance_NetDelay_high" -report_strategy {No Reports} -constrset constrs_1 -parent_run synth_1
 } else {
-  set_property strategy "Performance_ExplorePostRoutePhysOpt" [get_runs impl_1]
+  set_property strategy "Performance_NetDelay_high" [get_runs impl_1]
   set_property flow "Vivado Implementation 2018" [get_runs impl_1]
 }
 set obj [get_runs impl_1]
@@ -1845,11 +1845,11 @@ set_property -name "display_name" -value "impl_1_post_route_phys_opt_report_bus_
 }
 set obj [get_runs impl_1]
 set_property -name "part" -value "xc7z020clg400-1" -objects $obj
-set_property -name "strategy" -value "Performance_ExplorePostRoutePhysOpt" -objects $obj
+set_property -name "strategy" -value "Performance_NetDelay_high" -objects $obj
 set_property -name "steps.phys_opt_design.is_enabled" -value "1" -objects $obj
-set_property -name "steps.phys_opt_design.args.directive" -value "Explore" -objects $obj
-set_property -name "steps.route_design.args.more options" -value "-tns_cleanup" -objects $obj
+set_property -name "steps.phys_opt_design.args.directive" -value "AggressiveExplore" -objects $obj
 set_property -name "steps.post_route_phys_opt_design.is_enabled" -value "1" -objects $obj
+set_property -name "steps.post_route_phys_opt_design.args.directive" -value "AggressiveExplore" -objects $obj
 set_property -name "steps.write_bitstream.args.readback_file" -value "0" -objects $obj
 set_property -name "steps.write_bitstream.args.verbose" -value "0" -objects $obj
 
